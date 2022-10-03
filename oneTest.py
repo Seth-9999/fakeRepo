bb40280fe68e27cc839e7ed5b5df198846a80487
Seth-9999 <86006195+Seth-9999@users.noreply.github.com>
2022-10-03 00:18:11 -0400

    Create oneTest.py

---
+++ oneTest.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+ 
+ 
+def pprint():
+  lab["text"] = "You clicked"
+ 
+ 
+ 
+# ===== GUI ==== #
+root = tk.Tk()
+ 
+lab = tk.Label(root,
+  text="Hello World!", font="Arial 36")
+lab.grid(row=0, column=1)
+ 
+button = tk.Button(root, text="Click", font="Arial 36")
+button.grid(row=0, column=0)
+button["command"] = pprint
+ 
+root.mainloop()
